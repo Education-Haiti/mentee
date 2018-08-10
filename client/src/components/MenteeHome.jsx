554edc3e5d6fc1daa1dbeb0212dfcad97412ff5e
JP Vertil <vertil.jp@gmail.com>
2018-08-10 01:20:30 -0700

    do initial setup

---
+++ client/src/components/MenteeHome.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+class MenteeHome extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+
+		}
+	}
+
+	render() {
+		return (
+			<div>
+				I am a Mentee React Component rendering !!!
+			</div>
+		)
+	}
+}
+
+export default MenteeHome;
---
+++ client/src/index.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import MenteeHome from './components/MenteeHome.jsx';
+
+ReactDOM.render(<MenteeHome/>, document.getElementById('mentee'));
