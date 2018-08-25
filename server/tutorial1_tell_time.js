8d7ac91acce4f1c762a84de870754998a4f9501e
JP Vertil <vertil.jp@gmail.com>
2018-08-25 11:15:36 -0700

    tutorial to send messages to the websitetesting channel and read messages containing key words

---
+++ server/tutorial1_tell_time.js
@@ -0,0 +1,13 @@
+env = require('env2')('../.env');  // required to access the environment variables
+const { IncomingWebhook, WebClient } = require('@slack/client');
+console.log('Getting started with Slack Developer Kit for Node.js');
+
+const timeNotification = new IncomingWebhook(process.env.SLACK_WEBHOOK_URL);
+const currentTime = new Date().toTimeString();
+timeNotification.send(`The current time is ${currentTime}`, (error, resp) => {
+  if (error) {
+    return console.error(error);
+  }
+  console.log('Notification sent');
+});
+
