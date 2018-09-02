f79a49bd14584f33bccad16a0ab179eb01bc1aec
JP Vertil <vertil.jp@gmail.com>
2018-09-01 17:06:48 -0700

    connect checklist with database

--- client/src/components/Checklist.jsx
+++ client/src/components/Checklist.jsx
@@ -6,45 +6,67 @@ class Checklist extends React.Component {
             super(props);
             this.state = {
                 items: {
-                    'SAT' : false,
-                    'TOEFL': true
-                }
+                },
+                email: 'jvertil@nd.edu'
         }
     }
 
+    componentDidMount() {
+        this.getChecklist(this.state.email);
+    }
+
+    getChecklist(theEmail) {
+        axios.get(`/mentees/checklist/${theEmail}`)
+            .then((response) => {
+                this.setState({ items: response.data[0].checklist });
+            })
+            .catch((error) => {
+                console.log('Axios error in getting checklist : ', error);
+            })
+    }
+
+    updateChecklist(theEmail) {
+        axios.put(`/mentees/checklist/${theEmail}`, {
+            newChecklist: this.state.items
+        })
+        .then((response) => {
+            this.getChecklist(this.state.email);
+        })
+        .catch((error) => {
+            console.log('Axios-side error in updating checklist : ', error);
+        })
+
+    }
+
     handleCheckedItem (e) {
-        console.log(e.target.checked);
-        //if (e.target.checked === false) {
-            //this.setState()
-        //}
-        
+        console.log(e.target.name);
+        console.log(this.state.items);
+        let item = e.target.name;
+        let newItems = this.state.items; // cannot update nested objects in state. must modify current one and update
+        if (this.state.items[`${item}`] === false) {
+           newItems[`${item}`] = true;    
+        } else {
+           newItems[`${item}`] = false;
+        }
+        this.setState({items: newItems}, () => {
+            this.updateChecklist(this.state.email);
+        })
     }
 
     render() {
         return (
             <div>
-                 {/* {
-                     this.state.items.map((item, index) => {
-                         return <div>
-                                    <input type="checkbox" 
-                                        checked={item.done} 
-                                        name={item.title} 
-                                        onChange={this.handleCheckedItem.bind(this)}/>
-                                    {item.title}
-                                </div>
-                     })
-                 } */}
-                 {
-                     Object.keys(this.state.items).map((keyName, keyIndex) => {
-                         return <div>
-                                    <input type="checkbox" 
-                                        checked={this.state.items[`${keyName}`]} 
-                                        //name={item.title} 
-                                        onChange={this.handleCheckedItem.bind(this)}/>
-                                    {keyName}
-                               </div>
-                     })
-                 }
+                {
+                    Object.keys(this.state.items).map((keyName, keyIndex) => {
+                        return <div>
+                                <input type="checkbox" 
+                                    checked={this.state.items[`${keyName}`]} 
+                                    name={keyName} 
+                                    onChange={this.handleCheckedItem.bind(this)}/>
+                                {keyName}
+                            </div>
+                    })
+                }
             </div>
         )
     }
