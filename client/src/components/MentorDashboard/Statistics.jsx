d148be5a84c41ef262b3abc5b90d357bad4d3ea8
JP Vertil <vertil.jp@gmail.com>
2018-09-08 09:09:54 -0700

    retrieve all users on mentor dashboard

--- client/src/components/MentorDashboard/Statistics.jsx
+++ client/src/components/MentorDashboard/Statistics.jsx
@@ -1,11 +1,32 @@
 import  React from 'react';
-
+import axios from 'axios';
 class Statistics extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-
+            allUsers: {},
+            totalMentors : 0,
+            totalMentees: 0,
+            mentees3: 0,
+            mentees2: 0,
+            mentees1: 0,
+            menteesT: 0
         }
+    }
+
+    componentDidMount() {
+        this.getAllUsers();
+    }
+
+    getAllUsers() {
+        axios.get('/users')
+            .then((response) => {
+                console.log('hhhhhhhhh: ', response);
+                this.setState({ allUsers: response.data });
+            })
+            .catch((error) => {
+                console.log('Axios-sise error in retrieving users', error);
+            })
     }
 
     render () {
