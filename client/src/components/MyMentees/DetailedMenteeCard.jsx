a9a7d90ca5686c3b2096fb654274f90acfff5004
JP Vertil <vertil.jp@gmail.com>
2018-09-10 17:23:37 -0700

    render mentees in 3e on the MyMentees component

---
+++ client/src/components/MyMentees/DetailedMenteeCard.jsx
@@ -0,0 +1,42 @@
+import React from 'react';
+
+class DetailedMenteeCard extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+
+        }
+    }
+
+    componentDidMount() {     
+    }
+
+
+    render () {
+
+        return (
+            <div>
+              <div className="mentor-d-profile-card-container column">
+                <img className="mentor-d-profile-card-photo" src={this.props.displayPhoto}/>
+                <div className="mentor-d-profile-card-name">
+                    {this.props.mentee.full_name}
+                </div>
+
+                <div className="mentor-d-profile-details">
+                    <div> {this.props.mentee.hometown} </div>
+                    <div> {this.props.mentee.school}</div>
+                    <div> @{this.props.slackHandle}  </div>
+                </div>
+
+                <div className="mentor-d-profile-kudos column">
+                    {this.props.mentee.number_kudos_received} Kudos
+                </div>
+
+              </div>
+            </div>
+        )
+    }
+
+}
+
+export default DetailedMenteeCard;
--- client/src/components/MyMentees/MyMentees.jsx
+++ client/src/components/MyMentees/MyMentees.jsx
@@ -1,17 +1,67 @@
 import React from 'react';
 import axios from 'axios';
 import SECRETS from '../../client_secrets.js';
+import DetailedMenteeCard from './DetailedMenteeCard.jsx';
 
 class MyMentees extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
             myMentees : [],
-            mentees3: [],
-            mentees2: [],
-            mentees1: [],
             menteesT: [],
+            mentees1: [],
+            mentees2: [],
+            mentees3: [],
+            mentorsByEmail: {},
+            displayPhotos: {},
+            slackHandles: {},
         }
+    }
+
+    componentDidMount () {
+        this.initializeDisplayPhotosAndHandlesObj();
+        this.getMenteesByGrade('3');
+
+    }
+
+    initializeDisplayPhotosAndHandlesObj() {
+        axios.get(`https://slack.com/api/users.list?token=${SECRETS.BOT_TOKEN}`)
+			.then((response) => {
+                console.log('All users from slack !!!! : ', response.data.members);
+                // make an object whose key is the email of all users and value is the url to their photos
+                let tempObj = {};
+                let tempHandles = {};
+                let dataArray = response.data.members;
+                for (let i = 0; i < dataArray.length; i++) {
+                    tempObj[dataArray[i].profile.email] = dataArray[i].profile.image_512; 
+                    tempHandles[dataArray[i].profile.email] = dataArray[i].name;
+                }
+
+                this.setState({ displayPhotos: tempObj });
+                this.setState({ slackHandles: tempHandles });
+				
+            })
+            .catch((error) => {
+                console.log('Axios error in getting all users from SLACK API : ', error);
+            })
+    }
+
+    getMenteesByGrade (theGrade) {
+        axios.get(`/users/mentees/grade/${theGrade}`)
+            .then((response) => {
+                if (theGrade === 'T') {
+                    this.setState({ menteesT: response.data });
+                } else if (theGrade === '1') {
+                    this.setState({ mentees1: response.data });
+                } else if (theGrade === '2') {
+                    this.setState({ mentees2: response.data });
+                } else if (theGrade === '3') {
+                    this.setState({ mentees3: response.data });
+                }     
+            })
+            .catch((error) => {
+                console.log('Axios-error in getting mentees based on grade : ', error);
+            })
     }
 
     render () {
@@ -36,6 +86,18 @@ class MyMentees extends React.Component {
                 <div className="mentor-d-title column">
                     3e
                 </div>
+
+                <div className="peers-line-container">
+                    {
+                        this.state.mentees3.map((peer, index) => {
+                            return (
+                                <DetailedMenteeCard className="all-mentors-line-container" key={index} mentee={peer} displayPhoto={this.state.displayPhotos[peer.email]} slackHandle={this.state.slackHandles[peer.email]}/>
+                            )
+                        })
+                    }
+               </div>  
+
+
 
             </div>
         )
