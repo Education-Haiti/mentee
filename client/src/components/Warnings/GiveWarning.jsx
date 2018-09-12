7fd71791e9d5182a19f978a7f4e9750e7460e1d8
JP Vertil <vertil.jp@gmail.com>
2018-09-12 02:56:49 -0700

    implement frames for the GiveWarning component

--- client/src/components/Warnings/GiveWarning.jsx
+++ client/src/components/Warnings/GiveWarning.jsx
@@ -4,14 +4,43 @@ class GiveWarning extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
+            showGiveWarningButton: true,
+        }
+    }
 
+    toggleWarning () {
+        if (this.state.showGiveWarningButton === true) {
+            this.setState({ showGiveWarningButton: false });
+        } else {
+            this.setState({ showGiveWarningButton: true });
         }
     }
 
     render () {
+        let giveWarningButton = null; 
+        let giveWarningBody = null;
+
+        if (this.state.showGiveWarningButton === true) {
+            giveWarningButton = (
+                <button className="give-warning-button" onClick={this.toggleWarning.bind(this)}>
+                    GIVE WARNING
+                </button>
+            )
+        } else if (this.state.showGiveWarningButton === false) {
+            giveWarningBody = (
+                <div>
+                    <input className="give-warning-input"/>
+                    <div>
+                        <button className="give-warning-button"> Submit </button>
+                        <button className="give-warning-button" onClick={this.toggleWarning.bind(this)}> Cancel </button>
+                    </div>
+                </div>
+            )
+        }
         return (
-            <div>
-                This is the GiveWarning component, RENDERING !
+            <div className="give-warning-container">
+                {giveWarningButton}
+                {giveWarningBody}
             </div>
         )
     }
